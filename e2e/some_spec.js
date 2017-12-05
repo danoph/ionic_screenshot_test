51d1770dc2c91ac72ad108b1fc78d0e40f8af886
Daniel Errante <danoph@gmail.com>
2017-12-04 19:12:17 -0600

    tests running on android + ios

--- e2e/some_spec.js
+++ e2e/some_spec.js
@@ -1,9 +1,63 @@
 //import { browser, element, by, $, $$ } from 'protractor';
 var fs = require('fs');
+//var wd = require('wd');
 
 describe('something', () => {
+  //let webContext;
+  //var myBrowser = wd.promiseChainRemote();
+
+  beforeEach(() => {
+    //webContext = "WEBVIEW_io.ionic.starter";
+
+    webContext = wdBrowser.contexts().then(function (ctxs) {
+      console.log(ctxs); //This will give you list of all webviews in your app.
+      return wdBrowser.context(ctxs[1]); //'index' of your required WebView
+    })
+  });
+
   it('does stuff', () => {
+    browser.sleep(3000);
+
+    const input = $('input[type="text"]');
+
+    console.log('input', input);
+
+    input.sendKeys('hello!');
+
+    browser.sleep(3000);
+    //console.log('running test!');
+    //r processes. Original error: Unable to get pids for tomator2.server
+    //WebDriverError: No such context found.
+    //webContext.then(ctx => {
+      //console.log('ctx', ctx);
+    //});
+
+    //wdBrowser.contexts().then(function (ctxs) {
+      //console.log(ctxs); //This will give you list of all webviews in your app.
+      //return wdBrowser.context(ctxs[index]); //'index' of your required WebView
+    //})
+    //print(XCUIApplication().debugDescription)
+    //console.log(browser);
+    //console.log('wdBrowser', wdBrowser);
+    //console.log('myBrowsewr', myBrowser);
     //browser.get('');
+
+    //browser.sleep(1000).then(() => {
+      //console.log('done!');
+      //const usernameInput = wdBrowser.elementByAccessibilityId('txtUsername').then(el => {
+        //console.log('usernameInput', usernameInput);
+        //usernameInput.sendKeys('hello world');
+        //el.sendKeys('hello world');
+      //});
+    //});
+
+    //console.log('usernameInput', usernameInput);
+
+    //usernameInput.sendKeys('hello world');
+
+    //browser.sleep(5000).then(() => {
+      //console.log('done!');
+    //});
 
     //browser.takeScreenshot().then(png => {
       //const filename = 'test-ios';
@@ -13,6 +67,16 @@ describe('something', () => {
       //stream.end();
     //});
 
-    expect($('body').getText()).toEqual('hello');
+    //console.log('iosLogin'
+
+    //expect($('body').getText()).toEqual('hello');
   });
 });
+
+//export class LoginPage {
+  //usernameInput: $('.username');
+//}
+
+//export class IosLoginPage {
+  //usernameInput: browser.driver.elementByAccessibilityId('txtUsername')
+//}
--- protractor.conf.js
+++ protractor.conf.js
@@ -1,19 +1,33 @@
 // configuring wd in onPrepare
 // wdBridge helps to bridge wd driver with other selenium clients
 // See https://github.com/sebv/wd-bridge/blob/master/README.md
-exports.config = {
+
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+const iosConfig = {
   seleniumAddress: 'http://localhost:4723/wd/hub',
 
   specs: [
     'e2e/*_spec.js'
   ],
 
+  frameworks: ['jasmine'],
+
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
+  },
+
   // Reference: https://github.com/appium/sample-code/blob/master/sample-code/examples/node/helpers/caps.js
   capabilities: {
-    automationName: 'XCUITest',
-    app: '/Users/daniel/workspace/member-ionic3/platforms/ios/build/emulator/MyApp.app',
+    //automationName: 'XCUITest',
+    //automationName: 'Appium',
+    //app: '/Users/daniel/workspace/member-ionic3/platforms/ios/build/emulator/MyApp.app',
+    app: '/Users/danielerrante/workspace/centene/member-ionic3/platforms/ios/build/emulator/MyApp.app',
     browserName: '',
     //browserName: 'safari',
+    autoWebview: true,
     platformName: 'iOS',
     platformVersion: '11.1',
     deviceName: 'iPhone Simulator',
@@ -26,5 +40,88 @@ exports.config = {
       protractor = require('protractor'),
       wdBridge = require('wd-bridge')(protractor, wd);
     wdBridge.initFromProtractor(exports.config);
+
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
+
+const androidConfig = {
+  seleniumAddress: 'http://localhost:4723/wd/hub',
+
+  specs: [
+    'e2e/*_spec.js'
+  ],
+
+  frameworks: ['jasmine'],
+
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
+  },
+
+  // Reference: https://github.com/appium/sample-code/blob/master/sample-code/examples/node/helpers/caps.js
+  capabilities: {
+    browserName: '',
+    //browserName: 'chrome',
+    platformName: 'Android',
+    platformVersion: '7.0',
+    //platformVersion: '7.1.1',
+    //'appium-version': '1.7.1',
+    //automationName: "UiAutomator2",
+    //automationName: 'uiautomator2',
+    //deviceName: 'Android Emulator',
+    deviceName: 'android-25',
+    //'browserName': 'android'
+    //deviceName: '66c4518f',
+    autoWebview: true,
+    //autoWebviewTimeout: 20000,
+    //deviceName: 'emulator-5554',
+    //deviceName: 'android-25',
+    //appiumVersion: '1.7.1',
+    //deviceName: 'android-24-google_apis-x86_64-v24.4.1-wd-manager',
+    app: '/Users/danielerrante/workspace/centene/member-ionic3/platforms/android/build/outputs/apk/android-debug.apk'
+  },
+  onPrepare() {
+    var wd = require('wd'),
+      protractor = require('protractor'),
+      wdBridge = require('wd-bridge')(protractor, wd);
+    wdBridge.initFromProtractor(exports.config);
+    //require('ts-node').register({
+      //project: 'e2e/tsconfig.e2e.json'
+    //});
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  },
+
+  baseUrl: 'http://10.0.2.2:8000'
+};
+
+exports.config = androidConfig;
+//exports.config = iosConfig;
+//// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+//exports.config = {
+  //allScriptsTimeout: 11000,
+  //specs: [
+    //'./e2e/**/*.e2e-spec.ts'
+  //],
+  //capabilities: {
+    //'browserName': 'chrome'
+  //},
+  //directConnect: true,
+  //baseUrl: 'http://localhost:4201/',
+  //framework: 'jasmine',
+  //jasmineNodeOpts: {
+    //showColors: true,
+    //defaultTimeoutInterval: 30000,
+    //print: function() {}
+  //},
+  //onPrepare() {
+    //require('ts-node').register({
+      //project: 'e2e/tsconfig.e2e.json'
+    //});
+    //jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  //}
+//};
+
